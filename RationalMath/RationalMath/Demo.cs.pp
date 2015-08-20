00c563d1c08159d7008cad56f3439e51792de7ae
Venzhyk Andrey <fanatt20@gmail.com>
2015-08-20 22:02:42 +0300

    added nuget

---
+++ RationalMath/RationalMath/Demo.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  RationalMath;
+
+namespace $rootnamespace$
+{
+     class Demo
+     {
+         public void HowToUse()
+         {
+             var number1 = new Rational(25, 3);
+             var number2 = new Rational(-133, 12);
+             var number3 = new Rational(3);   
+
+             var sum = number1 + number2;
+             var minus = number1 - number2;
+             var div = number1 / number2;
+             var mul = number2*number1;
+             var doub = (double) minus;
+         }
+
+
+     }
+}
