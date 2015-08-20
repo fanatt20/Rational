6a54ffe6e7402e2f441a1d3088194d5a5cf76d72
Venzhyk Andrey <fanatt20@gmail.com>
2015-08-20 14:58:39 +0300

    passed tests

--- RationalMath/RationalMath.Tests/Rational.Tests.cs
+++ RationalMath/RationalMath.Tests/Rational.Tests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RationalMath.Tests
@@ -10,13 +9,13 @@ namespace RationalMath.Tests
         [TestMethod]
         public void TestCtor_DivideByZero_ThrowException()
         {
-            try {
+            try
+            {
                 var rat = new Rational(4, 0);
-                //if object created
-                Assert.Fail("");
+                Assert.Fail("Exception must be thrown in constructor ");
             }
-            catch(DivideByZeroException) {
-                //this exception is right
+            catch (DivideByZeroException)
+            {
                 Assert.IsTrue(true);
             }
         }
@@ -25,7 +24,6 @@ namespace RationalMath.Tests
         public void TestCtor_CorretcData_Created()
         {
             var rat = new Rational(10, 4);
-            //mean that we can create object with correct data
             Assert.IsTrue(true);
         }
 
@@ -33,25 +31,28 @@ namespace RationalMath.Tests
         public void TestProperties_SettingGetting_Passed()
         {
             var rat = new Rational(10, 4);
-            //values that passed in ctor was accociated with Numerator and Denominator
-            Assert.AreEqual(rat.Numerator, 10);
-            Assert.AreEqual(rat.Denominator, 4);
+
+            Assert.AreEqual(10, rat.Numerator);
+            Assert.AreEqual(4, rat.Denominator);
 
             rat.Denominator = -13;
-            Assert.AreEqual(rat.Denominator, -13);
+            Assert.AreEqual(-13, rat.Denominator);
+
             rat.Numerator = 0;
-            Assert.AreEqual(rat.Numerator, 0);
+            Assert.AreEqual(0, rat.Numerator);
         }
 
         [TestMethod]
         public void TestDenominator_SettingZero_thrownException()
         {
             var rat = new Rational(10, 7);
-            try {
+            try
+            {
                 rat.Denominator = 0;
-                Assert.Fail("");
+                Assert.Fail("Denominator is Changed to zero but exception wasn't throw");
             }
-            catch(DivideByZeroException) {
+            catch (DivideByZeroException)
+            {
                 Assert.IsTrue(true);
             }
         }
@@ -60,43 +61,46 @@ namespace RationalMath.Tests
         public void TestCompare_withDoubleFloatInt_Passed()
         {
             var rat = new Rational(625, 5);
-            Assert.AreEqual(125, (double)rat);
-            Assert.AreEqual(125, (float)rat);
-            Assert.AreEqual(125, (int)rat);
+            Assert.AreEqual(125, (double) rat, "Invalid cast to double");
+            Assert.AreEqual(125, (float) rat, "Invalid cast to float");
+            Assert.AreEqual(125, (int) rat, "Invalid cast to int");
         }
+
         [TestMethod]
         public void TestAdd_WithOtherRational_Passed()
         {
             var rat = new Rational(625, 5);
             var other = new Rational(338, 13);
             var sum = rat + other;
-            Assert.AreEqual(151, (double)sum);
+            Assert.AreEqual(151, (double) sum, "Invalid Sum operation");
         }
+
         [TestMethod]
         public void TestMinus_WithOtherRational_Passed()
         {
             var rat = new Rational(625, 5);
             var other = new Rational(338, 13);
             var minus = rat - other;
-            Assert.AreEqual(99, (double)minus);
+            Assert.AreEqual(99, (double) minus, "Invalid minus operation");
         }
+
         [TestMethod]
         public void TestMultiply_WithOtherRational_Passed()
         {
             var rat = new Rational(625, 5);
             var other = new Rational(338, 13);
-            var mult = rat * other;
-            Assert.AreEqual(3250, (double)mult);
+            var mult = rat*other;
+            Assert.AreEqual(3250, (double) mult, "Invalid multiply operation");
         }
+
         [TestMethod]
         public void TestDivide_WithOtherRational_Passed()
         {
-            var rat = new Rational(625, 5);
-            var other = new Rational(338, 13);
-            var div = rat / other;
-            Assert.AreEqual(125, div.Numerator);
-            Assert.AreEqual(26,div.Denominator);
+            var rat = new Rational(7, 5);
+            var other = new Rational(5, 7);
+            var div = rat/other;
+            Assert.AreEqual(49, div.Numerator, "Invalid divide operation");
+            Assert.AreEqual(25, div.Denominator, "Invalid divide operation");
         }
-        
     }
 }
--- RationalMath/RationalMath/Rational.cs
+++ RationalMath/RationalMath/Rational.cs
@@ -19,7 +19,7 @@ namespace RationalMath
             get { return _eps; }
             set
             {
-                if(value <= 0)
+                if (value <= 0)
                     throw new ArgumentOutOfRangeException("Eps must be non negative");
                 _eps = value;
             }
@@ -31,52 +31,76 @@ namespace RationalMath
         {
             get
             {
-                if(_denominator == 0)
+                if (_denominator == 0)
                     _denominator = 1;
                 return _denominator;
             }
             set
             {
-                if(value == 0)
+                if (value == 0)
                     throw new DivideByZeroException();
                 _denominator = value;
             }
         }
 
+        public override string ToString()
+        {
+            return Numerator + "/" + Denominator;
+        }
+
+        public static explicit operator Rational(string value)
+        {
+            var arr = value.Split('/');
+            var numerator = 0;
+            var denominator = 0;
+            if (arr.Length != 2)
+            {
+                if (arr.Length == 1 && int.TryParse(arr[0], out numerator))
+                    return new Rational(numerator);
+
+                throw new InvalidCastException();
+            }
+
+            if (!int.TryParse(arr[0], out numerator) || !int.TryParse(arr[1], out denominator))
+                throw new InvalidCastException();
+            return new Rational(numerator, denominator);
+        }
+
         public static Rational operator +(Rational val, Rational other)
         {
-            return new Rational(val.Numerator*other.Denominator+val.Denominator*other.Numerator,val.Denominator*other.Denominator);
-            
+            return new Rational(val.Numerator*other.Denominator + val.Denominator*other.Numerator,
+                val.Denominator*other.Denominator);
         }
 
         public static Rational operator -(Rational val, Rational other)
         {
-            return new Rational(val.Numerator * other.Denominator - val.Denominator * other.Numerator, val.Denominator * other.Denominator);
+            return new Rational(val.Numerator*other.Denominator - val.Denominator*other.Numerator,
+                val.Denominator*other.Denominator);
         }
 
         public static Rational operator *(Rational val, Rational other)
         {
-            return new Rational(val.Numerator*other.Numerator,val.Denominator*val.Denominator);
+            return new Rational(val.Numerator*other.Numerator, val.Denominator*other.Denominator);
         }
 
         public static Rational operator /(Rational val, Rational other)
         {
-            return  new Rational(val.Numerator*other.Denominator,val.Denominator*other.Numerator);
+            return new Rational(val.Numerator*other.Denominator, val.Denominator*other.Numerator);
         }
 
         public static explicit operator double(Rational val)
         {
-            return val.Numerator / val.Denominator;
+            return val.Numerator/val.Denominator;
         }
 
         public static explicit operator float(Rational val)
         {
-            return val.Numerator / val.Denominator;
+            return val.Numerator/val.Denominator;
         }
 
         public static explicit operator int(Rational val)
         {
-            return val.Numerator / val.Denominator;
+            return val.Numerator/val.Denominator;
         }
 
         public static bool operator ==(Rational val, Rational other)
@@ -88,6 +112,5 @@ namespace RationalMath
         {
             return val.Numerator != other.Numerator || val.Denominator != other.Denominator;
         }
-
     }
 }
