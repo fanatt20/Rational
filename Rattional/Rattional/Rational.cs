1265e0cf397d4d8d984005e70540003486e014e1
Venzhyk Andrey <fanatt20@gmail.com>
2015-08-20 12:22:34 +0300

    init

--- Rattional/Rattional/Rational.cs
+++ Rattional/Rattional/Rational.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Rattional
 {
-    public class Rational
+    public struct Rational
     {
-        int 
+        private int _numerator;
+        private int _denominator;
+        public int Numerator { get { return _numerator; } set { _numerator = value; } }
+        public int Denominati
+
+        public Rational(int numerator,int denominator )
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
     }
 }
